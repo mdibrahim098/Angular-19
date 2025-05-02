269419ce2e841fec06a2a937e59f975b6a0aeae4
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-05-02 16:23:02 +0600

    Change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TestComponent } from "./test/test.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone : true,
+  imports: [RouterOutlet, TestComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
