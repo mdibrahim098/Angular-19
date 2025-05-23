78be70ab26dc5b1d19015139e202c50546a2f96f
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-05-23 23:16:25 +0600

    Change 9

---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'course-card',
+  imports: [],
+  templateUrl: './course-card.component.html',
+  styleUrl: './course-card.component.css'
+})
+export class CourseCardComponent {
+
+  
+    @Input() title: string;
+    constructor() { }
+      ngOnInit() {
+      }
+
+
+
+}
