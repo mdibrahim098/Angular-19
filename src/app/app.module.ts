9099d702a2a434636921ff676cdb66c073ae4370
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-06-01 21:16:22 +0600

    Change 27

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CourseCardComponent } from "./course-card/course-card.component";
--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,35 +1,28 @@
-import { Component, Input, EventEmitter, Output, input } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { Course } from '../app.module';
 
 @Component({
   selector: 'course-card',
   templateUrl: './course-card.component.html',
-  styleUrl: './course-card.component.css'
+  styleUrls: ['./course-card.component.css']
 })
-export class CourseCardComponent {
-
-  
-    @Input()
-    course: Course;
-
-    @Input({required:true})
-    cardIndex :number;
-
-    
-    @Output()
-    courseSelected = new EventEmitter<Course>();
-
-     
-      ngOnInit() {
-      }
-
-      onCourseViewed() {
-        console.log("Hello");
-        this.courseSelected.emit(this.course);
-      }
 
 
+export class CourseCardComponent implements OnInit {
 
+  @Input()
+  course!: Course;
 
+  @Input({ required: true })
+  cardIndex!: number;
 
+  @Output()
+  courseSelected = new EventEmitter<Course>();
+
+  ngOnInit(): void { }
+
+  onCourseViewed(): void {
+    console.log("Hello");
+    this.courseSelected.emit(this.course);
+  }
 }
